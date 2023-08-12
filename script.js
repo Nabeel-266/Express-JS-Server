302a60d4132ac80c476fa3a0727285d68cba6b5c
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2023-08-12 21:00:16 +0500

    Basic Server Initialized with three api's

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hi")
+import express from 'express';
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome to Home page");
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send("Welcome to About page");
+})
+
+app.get('/contact', (req, res) => {
+    res.status(200).send("Welcome to Contact page");
+})
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`)
+})
